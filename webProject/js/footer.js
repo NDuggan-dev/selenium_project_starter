66a45b98f9f8f15484d85341a14d1026a5812da7
niall.duggan <NDuggan@qa.com>
2021-07-29 17:05:06 +0100

    footer js

--- webProject/js/footer.js
+++ webProject/js/footer.js
@@ -1,4 +1,8 @@
 (function()
 {
-    $("#footer-placeholder").load("./footer.html")
+    $("#footer-placeholder").load("./html/footer.html");
 })(jQuery);
+
+let linktwo = document.getElementById("footer-placeholder");
+console.log(linktwo);
+linktwo.setAttribute("class", "page-footer font-small special-color-dark pt-4");
